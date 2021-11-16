d92b06548a370b78730e7e97f0338c94f6db9ea3
fajar-ab <fajarfanz378@gmail.com>
2021-11-17 00:14:50 +0700

    03 lessons: contoh mencetak keluaran

---
+++ 03 - ekspresi dan masukan keluaran/jumlah.cpp
@@ -0,0 +1,13 @@
+#include <cstdio>
+
+int main() {
+    int a, b;
+
+    printf("masukkan nilai a: ");
+    scanf("%d", &a);
+    printf("masukkan nilai b: ");
+    scanf("%d", &b);
+
+    printf("hasil dari penjumlahan a dan b : %d", a +b);
+
+}
