bbab7e83643be52212da05c477d780259564fb6e
fajar-ab <fajarfanz378@gmail.com>
2021-11-16 19:05:29 +0700

    03 lessons: contoh operasi relasional

---
+++ 03 - ekspresi dan masukan keluaran/relasional.cpp
@@ -0,0 +1,15 @@
+#include <cstdio>
+
+int main() {
+
+    printf("1 > 2  : %d\n", 1 > 2);
+    printf("2 > 1  : %d\n", 2 > 1);
+    printf("1 < 2  : %d\n", 1 < 2);
+    printf("2 < 1  : %d\n", 2 < 1);
+    printf("1 == 1 : %d\n", 1 == 1);
+    printf("2 == 1 : %d\n", 2 == 1);
+    printf("1 >= 2 : %d\n", 1 >= 2);
+    printf("1 <= 1 : %d\n", 1 <= 1);
+    printf("1 != 1 : %d\n", 1 != 1);
+    printf("1 != 2 : %d\n", 1 != 2);
+}
