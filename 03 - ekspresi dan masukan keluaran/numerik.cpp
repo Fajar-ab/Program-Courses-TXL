760d852f877d986efc7e0b727c35475bb4504257
fajar-ab <fajarfanz378@gmail.com>
2021-11-16 19:02:06 +0700

    03 lessons: contoh operasi prioritas pengerjaan

---
+++ 03 - ekspresi dan masukan keluaran/numerik.cpp
@@ -0,0 +1,13 @@
+#include <cstdio>
+
+int hasil1, hasil2;
+
+int main() {
+
+    hasil1 = 3 + 5 / 4;
+    hasil2 = (3 + 5) / 4;
+
+    printf("hasil1 = %d\n", hasil1);
+    printf("hasil2 = %d\n", hasil2);
+    
+}
