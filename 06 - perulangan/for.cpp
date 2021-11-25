442be825563dc2d82e60df242b05f46f991a4d98
fajar-ab <fajarfanz378@gmail.com>
2021-11-25 09:53:48 +0700

    06 lessons: contoh perulangan ke atas

---
+++ 06 - perulangan/for.cpp
@@ -0,0 +1,13 @@
+#include <cstdio>
+
+int main() {
+
+    int n;
+    scanf("%d", &n);
+
+    for(int i = 0; i < n; i++) {
+        printf("tulisan ini dicetak saat i = %d\n", i);
+    }
+
+    printf("akhir dari perogram\n");
+}
