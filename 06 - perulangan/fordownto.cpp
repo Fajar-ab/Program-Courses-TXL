3dd60d1514cc731de01fcdbc3865e9a52762a191
fajar-ab <fajarfanz378@gmail.com>
2021-11-25 10:01:19 +0700

    06 lessons: contoh perulangan for ke bawah

---
+++ 06 - perulangan/fordownto.cpp
@@ -0,0 +1,13 @@
+#include  <cstdio>
+
+int main() {
+	
+	int n;
+	scanf("%d", &n);
+
+	for(int i = n - 1; i >= 0; i--) {
+		printf("tulisan ini dicetak saat i = %d\n", i);
+	}
+
+	printf("akhir dari  perogram");
+}
