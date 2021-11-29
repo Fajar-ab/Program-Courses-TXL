2d459d6ac782d69c2bcca4d1637ad18a35944ddc
fajar-ab <fajarfanz378@gmail.com>
2021-11-29 20:41:35 +0700

    06 lessons: contoh perulangan dengan while

---
+++ 06 - perulangan/while.cpp
@@ -0,0 +1,15 @@
+#include <cstdio>
+
+int main() {
+
+    int n;
+    scanf("%d", &n);
+
+    int i = 0;
+    while(i < n) {
+        printf("tulisan ini dicetak selama i = %d\n", i);
+        i++;
+    }
+
+    printf("akhir dari perogram");
+}
