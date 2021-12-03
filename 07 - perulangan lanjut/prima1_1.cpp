d2c8165b921212da5734d4f8033fb195df83733f
fajar-ab <fajarfanz378@gmail.com>
2021-12-03 18:53:10 +0700

    07 lessons: contoh bilangan prima 1

---
+++ 07 - perulangan lanjut/prima1_1.cpp
@@ -0,0 +1,19 @@
+#include <cstdio>
+
+int main() {
+
+    int n;
+    scanf("%d", &n);
+    bool prima = true;
+    for (int i = 2; i <= n-1; i++) {
+        if (n % i == 0) {
+            prima = false;
+        }
+    }
+
+    if (prima) {
+            printf("%d adalah bilangan prima\n", n);
+    } else {
+    printf("%d bukan bilangan prima\n", n);
+    }
+}
